ee59e19a4607e7d9c9e88939c25ef4795f1b8d51
Patross <patrykt884@gmail.com>
2020-05-04 14:46:33 +0200

    Zadanie 1
    Signed-off-by: Patross <patrykt884@gmail.com>

--- src/Animal.java
+++ src/Animal.java
@@ -1,8 +1,43 @@
 import java.io.File;
 
 public class Animal {
-    public String species;
-    public Double weight;
+    static final public Double DEFAULT_DOG_WEIGHT = 10.0;
+    static final public Double DEFAULT_LION_WEIGHT = 190.0;
     public String name;
     File pic;
+    static final public Double DEFAULT_MOUSE_WEIGHT = 0.05;
+    final public String species;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        switch (species) {
+            case "lion":
+                this.weight = DEFAULT_LION_WEIGHT;
+                break;
+            case "mouse":
+                this.weight = DEFAULT_MOUSE_WEIGHT;
+                break;
+            case "dog":
+                this.weight = DEFAULT_DOG_WEIGHT;
+                break;
+        }
+    }
+
+    void feed() {
+        if (weight <= 0) System.out.println("sorry your pet is dead");
+        else {
+            weight += 1;
+            System.out.println("omomom");
+        }
+    }
+
+    void takeForAWalk() {
+        if (weight <= 0) System.out.println("sorry your pet is dead");
+        else {
+            weight -= 1;
+            System.out.println("RUUUUUN");
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
+        Animal dog = new Animal("dog");
+        dog.name = "Pieseł";
+        System.out.println(dog.name);
 
+        dog.feed();
+        dog.takeForAWalk();
     }
 
 }
